943da480ea790acd96ab21b18c643174682f83f8
Steve Averty <steve.averty1992@gmail.com>
2019-07-18 14:55:55 +0200

    Add topic feature

--- src/FirebaseNotification.php
+++ src/FirebaseNotification.php
@@ -6,11 +6,22 @@ use LaravelFCM\Message\OptionsBuilder;
 use LaravelFCM\Message\PayloadDataBuilder;
 use LaravelFCM\Message\PayloadNotificationBuilder;
 use FCM;
-use App\User;
+use LaravelFCM\Message\Topics;
 
 class FirebaseNotification
 {
 
+
+    /**
+     * Send notification to many users
+     *
+     * @param Collection $users
+     * @param string $title Title of the message
+     * @param string $body Body of the message
+     * @param array $data Data of the mesage
+     * @param string $action Call to actio nwhen user click on the notification
+     * @return void
+     */
     public static function sendTo($users, $title, $body, $data = null, $action = null){
         $tokens = [];
         $data = ($data === null) ? [] : $data;
@@ -25,6 +36,16 @@ class FirebaseNotification
 
     }
 
+    /**
+     * Send notification to a specific user
+     *
+     * @param Collection $users
+     * @param string $title Title of the message
+     * @param string $body Body of the message
+     * @param array $data Data of the mesage
+     * @param string $action Call to actio nwhen user click on the notification
+     * @return void
+     */
     public static function sendToUser($user, $title, $body, $data = null, $action = null){
         $data = ($data === null) ? [] : $data;
 
@@ -35,6 +56,17 @@ class FirebaseNotification
         self::send($tokens, $title, $body, $data, $action);
     }
 
+    /**
+     * Function that send the message
+     * This function is used in the sendTo function and in FirebaseNotificationTrait
+     *
+     * @param array $tokens List of FCM tokens
+     * @param string $title Title of the message
+     * @param string $body Body of the message
+     * @param array $data Data sent in the notification
+     * @param string $action Call to action
+     * @return void
+     */
     public static function send($tokens, $title, $body, $data, $action){
 
 
@@ -57,5 +89,18 @@ class FirebaseNotification
         $data = $dataBuilder->build();
 
         FCM::sendTo($tokens, $option, $notification, $data);
+    }
+
+    public static function sendToTopic($topic, $title, $body, $data = null, $action = null){
+        $notificationBuilder = new PayloadNotificationBuilder($title);
+        $notificationBuilder->setBody($body)
+                            ->setSound('default');
+
+        $notification = $notificationBuilder->build();
+
+        $topic = new Topics();
+        $topic->topic($topic);
+
+        FCM::sendToTopic($topic, null, $notification, $data);
     }
 }
